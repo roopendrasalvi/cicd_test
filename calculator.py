fe9c4efd78e318c32197225fee3831b1fe108ce7
roopendrasalvi <roopendrasalvi@gmail.com>
2026-04-20 14:51:24 +0530

    added solid check

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from typing import Union
+import math
 
 class Calculator:
     def add(self, a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
@@ -14,3 +15,10 @@ class Calculator:
         if b == 0:
             raise ValueError("Division by zero is not allowed")
         return a / b
+
+if __name__ == '__main__':
+    calc = Calculator()
+    print(calc.add(5, 3))        # Output: 8
+    print(calc.subtract(5, 3))   # Output: 2
+    print(calc.multiply(5, 3))   # Output: 15
+    print(calc.divide(5, 3))     # Output: 1.6666666666666667
